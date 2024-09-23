6e84c1ba9df98f1e5badc5c82090b9e72ea9db87
turgunov24 <learningplatforms.turgunov@gmail.com>
2024-09-23 11:02:16 +0500

    optimized

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { Joi, validate, ValidationError } from 'express-validation'
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
@@ -12,31 +11,6 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-
-const loginValidation = {
-  query: Joi.object({
-    id: Joi.string().required(),
-  }),
-  body: Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-  }).options({ abortEarly: false }),
-}
-app.post(
-  '/validation',
-  validate(loginValidation, { keyByField: true }, {}),
-  (req: Request, res: Response) => {
-    res.json({ s: 'success' })
-  }
-)
-
-app.use(function (err: any, req: any, res: any, next: any) {
-  if (err instanceof ValidationError) {
-    return res.status(err.statusCode).json(err)
-  }
-
-  return res.status(500).json(err)
-})
 
 app.listen(port, async () => {
   try {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { ValidationError } from 'express-validation'
 
 import referenceMainRoutes from './reference/main'
 import usersRoutes from './users'
@@ -9,5 +10,13 @@ const router = express.Router()
 router.use(authRoutes)
 router.use(usersRoutes)
 router.use(referenceMainRoutes)
+
+router.use((err: ValidationError | Error, req: Request, res: Response) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err)
+  }
+
+  return res.status(500).json({ message: err.message })
+})
 
 export default router
